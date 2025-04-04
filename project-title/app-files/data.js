54a85a96001b95958ea0db67bada39c84ff9bce5
parabiomech <parabiomech@gmail.com>
2025-04-04 21:08:57 +0900

    f
    Commit to main

---
+++ project-title/app-files/data.js
@@ -0,0 +1,588 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gs__3408",
+      "name": "GS__3408",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.0029625877803578504,
+        "pitch": 0.06699700942552589,
+        "fov": 1.3770865629940496
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.024178093117267707,
+          "pitch": 0.16164636025858314,
+          "rotation": 0,
+          "target": "1-gs__3409"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-gs__3409",
+      "name": "GS__3409",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03489636934745377,
+          "pitch": 0.31816218911146166,
+          "rotation": 0,
+          "target": "2-gs__3410"
+        },
+        {
+          "yaw": 3.105449803065399,
+          "pitch": 0.29836265906097736,
+          "rotation": 0,
+          "target": "0-gs__3408"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-gs__3410",
+      "name": "GS__3410",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.6253100414118791,
+        "pitch": 0.3589492625976263,
+        "fov": 1.3770865629940496
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4429535978006367,
+          "pitch": 0.23655092084868912,
+          "rotation": 0,
+          "target": "3-gs__3411"
+        },
+        {
+          "yaw": 0.1784361503496843,
+          "pitch": 0.4186089388771208,
+          "rotation": 0,
+          "target": "11-gs__3402"
+        },
+        {
+          "yaw": 3.129401439683555,
+          "pitch": 0.39706972049595635,
+          "rotation": 0,
+          "target": "1-gs__3409"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.20096079969641423,
+          "pitch": 0.28036403854376246,
+          "title": "SSCNAD",
+          "text": "사무실"
+        }
+      ]
+    },
+    {
+      "id": "3-gs__3411",
+      "name": "GS__3411",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.663809092252622,
+          "pitch": 0.652665343839093,
+          "rotation": 0,
+          "target": "4-gs__3412"
+        },
+        {
+          "yaw": -0.18407386859202823,
+          "pitch": 0.36426056978910637,
+          "rotation": 0,
+          "target": "5-gs__3413"
+        },
+        {
+          "yaw": 1.5589629385257782,
+          "pitch": 0.28999826429874176,
+          "rotation": 0,
+          "target": "2-gs__3410"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1601349634729381,
+          "pitch": 0.2043115136217324,
+          "title": "SSCNAD",
+          "text": "상담실"
+        }
+      ]
+    },
+    {
+      "id": "4-gs__3412",
+      "name": "GS__3412",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.7573440044028779,
+        "pitch": 0.29315104556066274,
+        "fov": 1.3770865629940496
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5254445854110203,
+          "pitch": 0.6341444499845519,
+          "rotation": 0,
+          "target": "3-gs__3411"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.20165255901728152,
+          "pitch": -0.14823789599395099,
+          "title": "SSCNAD",
+          "text": "원격지원실"
+        }
+      ]
+    },
+    {
+      "id": "5-gs__3413",
+      "name": "GS__3413",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.035298792760212194,
+        "pitch": 0.4664325455302425,
+        "fov": 1.3770865629940496
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.370242527801368,
+          "pitch": 0.5634829944368107,
+          "rotation": 0,
+          "target": "3-gs__3411"
+        },
+        {
+          "yaw": 0.9188014902052828,
+          "pitch": -0.12902177864762976,
+          "rotation": 0,
+          "target": "6-gs__3414"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.03390148199827969,
+          "pitch": 0.5235862639442495,
+          "title": "SSCNAD",
+          "text": "상담부스"
+        }
+      ]
+    },
+    {
+      "id": "6-gs__3414",
+      "name": "GS__3414",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9611246055521558,
+          "pitch": 0.38458235475470914,
+          "rotation": 0,
+          "target": "5-gs__3413"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.020211903790542607,
+          "pitch": 0.6274193264093029,
+          "title": "SSCNAD",
+          "text": "회의실&nbsp;"
+        },
+        {
+          "yaw": 1.5753876141866936,
+          "pitch": 0.33586463875054484,
+          "title": "창고",
+          "text": "크기 : 5X10(m)"
+        }
+      ]
+    },
+    {
+      "id": "7-gs__3416",
+      "name": "GS__3416",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.017776771821143456,
+          "pitch": 0.29267127347015,
+          "rotation": 0,
+          "target": "8-gs__3417"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.680738118667737,
+          "pitch": -0.11573684151590058,
+          "title": "대한장애인체육회",
+          "text": "체육관 &amp; 행정동"
+        },
+        {
+          "yaw": 0.41813711238977724,
+          "pitch": 0.014381957602612516,
+          "title": "대한장애인체육회",
+          "text": "교육동"
+        }
+      ]
+    },
+    {
+      "id": "8-gs__3417",
+      "name": "GS__3417",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.047858123785626816,
+          "pitch": 0.11506602994247572,
+          "rotation": 0,
+          "target": "9-gs__3418"
+        },
+        {
+          "yaw": -3.1194037136826562,
+          "pitch": 0.16232436954328833,
+          "rotation": 0,
+          "target": "7-gs__3416"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-gs__3418",
+      "name": "GS__3418",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04653022682026631,
+          "pitch": 0.45532700873192233,
+          "rotation": 0,
+          "target": "10-gs__3419"
+        },
+        {
+          "yaw": -1.627198034737173,
+          "pitch": 0.17323884700390835,
+          "rotation": 0,
+          "target": "8-gs__3417"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3829957057952189,
+          "pitch": -0.17994924590219696,
+          "title": "SSCNAD",
+          "text": "2층"
+        }
+      ]
+    },
+    {
+      "id": "10-gs__3419",
+      "name": "GS__3419",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6732097160718435,
+          "pitch": 0.5004515566485139,
+          "rotation": 0,
+          "target": "9-gs__3418"
+        },
+        {
+          "yaw": -0.6567257079745001,
+          "pitch": 0.384633439696902,
+          "rotation": 0,
+          "target": "0-gs__3408"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5713357819074059,
+          "pitch": 0.5239610884625083,
+          "title": "엘레베이터",
+          "text": "1층버튼"
+        },
+        {
+          "yaw": -0.5330379065632656,
+          "pitch": 0.34164360930085813,
+          "title": "엘레베이터",
+          "text": "2층버튼"
+        }
+      ]
+    },
+    {
+      "id": "11-gs__3402",
+      "name": "GS__3402",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.4006047310233072,
+        "pitch": -0.5814748412471058,
+        "fov": 1.3770865629940496
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
